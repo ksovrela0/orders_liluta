be14b6c4847931a39484dbb030cb3216deb0c1e3
ksovrela0 <g.ksovreli@psp.ge>
2022-07-11 15:57:55 +0400

    asdasd

--- server-side/writes.action.php
+++ server-side/writes.action.php
@@ -2371,7 +2371,7 @@ switch ($act){
                             FROM 	orders
                             JOIN	order_status ON order_status.id = orders.status_id
                             WHERE 	orders.actived = 1 $mine
-                            ORDER BY id DESC");
+                            ORDER BY orders.id DESC");
 
 
 
